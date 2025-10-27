006e3da086c0b3fb62b338d830ead330f46876da
DrGashi <drengashids@gmail.com>
2025-10-27 18:59:33 +0100

    .

--- function.php
+++ function.php
@@ -3,8 +3,8 @@
         wp_enqueue_style("style", get_stylesheet_url());
         wp_enqueue_style("style", get_template_directory_url() ."style.css", array(),"1.1", "all" );
         wp_enqueue_script("script", get_template_directory_url() ."script.js", array(), 1.1, true );
-        if(is_singular() && coments_open() && get_option("thread_coments")){
-            wp_enqueue_script("coment-reply");
+        if(is_singular() && comments_open() && get_option("thread_comments")){
+            wp_enqueue_script("comment-reply");
         }
     }
     add_action("wp_enqueue_scripts","add_theme_scripts");
