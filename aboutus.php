2d16e28413bea8c2b74be265b7ab150525837f2e
DrGashi <drengashids@gmail.com>
2025-11-17 19:16:23 +0100

    .

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: About Us
+Template Post Type: page
+*/
+?>
+<?php get_header();?>
+<section>
+    <h2>About Us</h2>
+    <p>hello this is us from ds</p>
+</section>
+<?php get_footer(); ?>
